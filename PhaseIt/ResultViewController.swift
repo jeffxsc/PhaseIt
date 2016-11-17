1fc702a3e0e80482147623330865f26036e0e1b8
Xiaocheng Yang <xiaocheng.yang@berkeley.edu>
2016-11-17 00:25:42 -0800

    Variables linked

--- PhaseIt/ResultViewController.swift
+++ PhaseIt/ResultViewController.swift
@@ -1,25 +1,31 @@
 //
-//  FirstViewController.swift
+//  ResultViewController.swift
 //  PhaseIt
 //
-//  Created by Xiaocheng Yang on 11/12/16.
+//  Created by Xiaocheng Yang on 11/13/16.
 //  Copyright © 2016 Berkly. All rights reserved.
 //
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class ResultViewController: UIViewController {
+    
+    var department = "."
+    
+    @IBOutlet weak var departmentOut: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        departmentOut.text = department;
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
